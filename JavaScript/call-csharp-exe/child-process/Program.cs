82c372494f9e58d163652d7863fa343bc39d5019
HiLow <newbie_wisdom@hotmail.com>
2023-05-07 23:52:31 +0900

    update

---
+++ JavaScript/call-csharp-exe/child-process/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"This Process was Called.");
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine($"args{i + 1} = {args[i]}");
+        }
+
+        Console.WriteLine("Final line can be used as return value.");
+    }
+}
--- JavaScript/call-csharp-exe/index.js
+++ JavaScript/call-csharp-exe/index.js
@@ -2,7 +2,7 @@ import { promisify } from 'util';
 import { execFile } from 'child_process';
 
 const execFileAsync = promisify(execFile);
-const execFilePath = './bin/ExecutableFile.exe';
+const execFilePath = './child-process/bin/Debug/net7.0/child-process.exe';
 const result = await execFileAsync(execFilePath, ['arg1', 'arg2']);
 
 console.log(result.stdout);
