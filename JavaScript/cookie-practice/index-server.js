8ce73db389c4928e3a600ba97a81467963661b31
HiLow <newbie_wisdom@hotmail.com>
2023-07-07 14:36:59 +0900

    add

---
+++ JavaScript/cookie-practice/index-server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const port = 5000;
+
+app.use(cookieParser());
+
+app.get('/', async (req, res) => {
+    res.cookie('last-access', Date.now(), {
+        maxAge: 60000, // (valid within 60 seconds)
+        httpOnly: true
+    });
+
+    res.cookie('passport', true, {
+        expires: new Date(Date.now() + 10000), // (valid within 10 seconds)
+        httpOnly: true
+    });
+
+    console.log(req.cookies);
+
+    const message = req.cookies['passport'] ? 'accessed within 10 seconds.' : 'first time accessed.';
+    res.status(200).send(message);
+});
+
+app.listen(port, () => {
+    console.log(`start on port ${port}`);
+});
