c4bed1d2bf5cec387f2582952a99350c695cb25e
HiLow <newbie_wisdom@hotmail.com>
2023-07-14 22:25:06 +0900

    update

--- Python/flask/app.py
+++ Python/flask/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return 'Hello Flask'
 
-if __name__ == "__main__":
+@app.route('/html')
+def html():
+    return render_template('index.html', title='Hello Flask', name='Guest')
+
+@app.route('/json')
+def json():
+    return jsonify({ 'message' : 'Hello Flask'})
+
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
