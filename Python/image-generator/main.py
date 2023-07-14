b92e5a5b09e49236a5b5173115dc57986fe38be6
HiLow <newbie_wisdom@hotmail.com>
2023-07-15 00:03:46 +0900

    clean

--- Python/image-generator/main.py
+++ Python/image-generator/main.py
@@ -5,9 +5,9 @@ width = 32
 height = 32
 threshold = 200
 
-image = Image.open('img/img.png')
-image.convert('L').point(lambda x: 0 if x<=threshold else 255).show()
+image = Image.open("img/img.png")
+image.convert("L").point(lambda x: 0 if x <= threshold else 255).show()
 
 rows, cols = generate(image, width, height, threshold)
 
-print({ 'rows': rows, 'cols': cols })
+print({"rows": rows, "cols": cols})
