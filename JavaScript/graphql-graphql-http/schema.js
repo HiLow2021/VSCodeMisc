07f5dc72f18fa9ca950bb2c1ac39514ab3846dd2
HiLow <newbie_wisdom@hotmail.com>
2023-09-23 01:51:40 +0900

    update

--- JavaScript/graphql-graphql-http/schema.js
+++ JavaScript/graphql-graphql-http/schema.js
@@ -4,16 +4,16 @@ import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
  * Construct a GraphQL schema and define the necessary resolvers.
  *
  * type Query {
- *   hello: String
+ *   ping: String
  * }
  */
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         fields: {
-            hello: {
+            ping: {
                 type: GraphQLString,
-                resolve: () => 'world'
+                resolve: () => 'hello world'
             }
         }
     })
