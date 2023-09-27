6aaca23a32dbfef2902dfda502170415184a1f19
HiLow <newbie_wisdom@hotmail.com>
2023-09-27 13:08:24 +0900

    update

--- JavaScript/get-weather-svg/index.js
+++ JavaScript/get-weather-svg/index.js
@@ -1,11 +1,14 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
 
-fs.mkdirSync('./svg/', { recursive: true });
+const outDirectory = './out/';
+if (!fs.existsSync(outDirectory)) {
+    fs.mkdirSync(outDirectory);
+}
 
 for (let index = 0; index < 1000; index++) {
     const response = await fetch(`https://www.jma.go.jp/bosai/forecast/img/${index}.svg`);
     if (response.ok) {
-        response.body.pipe(fs.createWriteStream(`./svg/${index}.svg`));
+        response.body.pipe(fs.createWriteStream(`${outDirectory}${index}.svg`));
     }
 }
