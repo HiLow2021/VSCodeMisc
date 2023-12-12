84710654949746cd8f7dcef16d536940e7d7097c
HiLow <newbie_wisdom@hotmail.com>
2023-12-13 00:08:07 +0900

    clean

--- CSharp/Playwright/Program.cs
+++ CSharp/Playwright/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Playwright;
 
-using var playwright = await Playwright.CreateAsync();
-await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
-var page = await browser.NewPageAsync();
-await page.GotoAsync("https://playwright.dev/dotnet");
-await page.ScreenshotAsync(new() { Path = "screenshot.png" });
+class PlaywrightSample
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://playwright.dev/dotnet");
+        await page.ScreenshotAsync(new() { Path = "screenshot.png" });
+    }
+}
