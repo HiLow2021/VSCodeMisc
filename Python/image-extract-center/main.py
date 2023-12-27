20a583f88177e1bedadad06eaef2328f387597f7
HiLow <newbie_wisdom@hotmail.com>
2023-12-28 03:24:08 +0900

    update

--- Python/image-extract-center/main.py
+++ Python/image-extract-center/main.py
@@ -9,13 +9,10 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 
-width = 800
-height = 800
-
 for filename in os.listdir(image_dir):
     if filename.lower().endswith((".png", ".jpg", ".jpeg", ".bmp")):
         input_image_path = os.path.join(image_dir, filename)
-        cropped_image = extract_center(input_image_path, width, height)
+        cropped_image = extract_center(input_image_path)
         output_image_path = os.path.join(output_dir, f"cropped_{filename}")
 
         cv2.imwrite(output_image_path, cropped_image)
--- Python/image-extract-center/utils.py
+++ Python/image-extract-center/utils.py
@@ -1,14 +1,23 @@
 import cv2
 
 
-def extract_center(image_path, width, height):
+def extract_center(image_path):
     img = cv2.imread(image_path)
-    center_x = img.shape[1] // 2
-    center_y = img.shape[0] // 2
-    x1 = center_x - width // 2
-    x2 = center_x + width // 2
-    y1 = center_y - height // 2
-    y2 = center_y + height // 2
+    width = img.shape[1]
+    height = img.shape[0]
+    center_x = width // 2
+    center_y = height // 2
+    range_x, range_h = (
+        (height, height)
+        if width > height
+        else (width, width)
+        if height > width
+        else (width, height)
+    )
+    x1 = center_x - range_x // 2
+    x2 = center_x + range_x // 2
+    y1 = center_y - range_h // 2
+    y2 = center_y + range_h // 2
     cropped_img = img[y1:y2, x1:x2]
 
     return cropped_img
