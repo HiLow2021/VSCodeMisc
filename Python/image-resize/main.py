80069b1d9d6236a60dd164172c675b599af1757e
HiLow <newbie_wisdom@hotmail.com>
2023-12-28 03:32:04 +0900

    update

---
+++ Python/image-resize/main.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+
+image_dir = "./media/"
+output_dir = "./out/"
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+width = 800
+height = 800
+
+for filename in os.listdir(image_dir):
+    if filename.lower().endswith((".png", ".jpg", ".jpeg", ".bmp")):
+        input_image_path = os.path.join(image_dir, filename)
+        image = Image.open(input_image_path)
+        resized_image = image.resize((width, height))
+        output_image_path = os.path.join(output_dir, f"resized_{filename}")
+
+        resized_image.save(output_image_path, quality=100)
