445d70e269e2cfa411ded7c2f9080a56bc6cec92
HiLow <newbie_wisdom@hotmail.com>
2024-03-12 00:20:03 +0900

    update

--- JavaScript/prisma-practice-seed/index.js
+++ JavaScript/prisma-practice-seed/index.js
@@ -5,18 +5,22 @@ const prisma = new PrismaClient();
 
 async function seed() {
     try {
+        await prisma.users.deleteMany({});
         await prisma.users.createMany({
             data: users
         });
 
+        await prisma.customers.deleteMany({});
         await prisma.customers.createMany({
             data: customers
         });
 
+        await prisma.invoices.deleteMany({});
         await prisma.invoices.createMany({
             data: invoices
         });
 
+        await prisma.revenue.deleteMany({});
         await prisma.revenue.createMany({
             data: revenue
         });
