d72785faea0ce1fee5ceea614ece5ea7ccedee93
HiLow <newbie_wisdom@hotmail.com>
2024-03-23 18:51:48 +0900

    add

---
+++ JavaScript/nextjs-app-mui-form/src/app/components/sideNavigation.tsx
@@ -0,0 +1,47 @@
+import { Home, Apps } from '@mui/icons-material';
+
+import Link from 'next/link';
+
+const links = [
+    { name: 'Home', href: '/', icon: Home },
+    { name: 'Page', href: '/page', icon: Apps }
+];
+
+const SideNavigation = () => {
+    return (
+        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+            <Link className="mb-2 flex h-20 justify-center items-center rounded-md bg-blue-600 p-4 md:h-40" href="/">
+                <div className={'flex flex-row justify-center text-white text-5xl leading-snug'}>
+                    <p>Next.js Practice</p>
+                </div>
+            </Link>
+            <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+                <Links />
+                <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+            </div>
+        </div>
+    );
+};
+
+const Links = () => {
+    return (
+        <>
+            {links.map((link) => {
+                const LinkIcon = link.icon;
+
+                return (
+                    <Link
+                        key={link.name}
+                        href={link.href}
+                        className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+                    >
+                        <LinkIcon className="w-6" />
+                        <p className="hidden md:block">{link.name}</p>
+                    </Link>
+                );
+            })}
+        </>
+    );
+};
+
+export default SideNavigation;
---
+++ JavaScript/nextjs-app-mui-form/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import '@/style/globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import SideNavigation from './components/sideNavigation';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'Next.js Practice',
+    description: 'Next.js Practice'
+};
+
+export default function RootLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="ja">
+            <body className={inter.className}>
+                <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+                    <div className="w-full flex-none md:w-64">
+                        <SideNavigation />
+                    </div>
+                    <div className="flex-grow p-2 md:overflow-y-auto">{children}</div>
+                </div>
+            </body>
+        </html>
+    );
+}
