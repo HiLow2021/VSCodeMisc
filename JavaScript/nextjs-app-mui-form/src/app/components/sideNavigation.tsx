077e65886d057095e64212a6923da9beb54d6488
HiLow <newbie_wisdom@hotmail.com>
2024-03-23 19:49:24 +0900

    update

--- JavaScript/nextjs-app-mui-form/src/app/components/sideNavigation.tsx
+++ JavaScript/nextjs-app-mui-form/src/app/components/sideNavigation.tsx
@@ -2,18 +2,11 @@ import { Home, Apps } from '@mui/icons-material';
 
 import Link from 'next/link';
 
-const links = [
-    { name: 'Home', href: '/', icon: Home },
-    { name: 'Page', href: '/page', icon: Apps }
-];
-
 const SideNavigation = () => {
     return (
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
             <Link className="mb-2 flex h-20 justify-center items-center rounded-md bg-blue-600 p-4 md:h-40" href="/">
-                <div className={'flex flex-row justify-center text-white text-5xl leading-snug'}>
-                    <p>Next.js Practice</p>
-                </div>
+                <Title />
             </Link>
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
                 <Links />
@@ -23,7 +16,20 @@ const SideNavigation = () => {
     );
 };
 
+const Title = () => {
+    return (
+        <div className={'flex flex-row justify-center text-white text-5xl leading-snug'}>
+            <p>Next.js Practice</p>
+        </div>
+    );
+};
+
 const Links = () => {
+    const links = [
+        { name: 'Home', href: '/', icon: Home },
+        { name: 'Form', href: '/form', icon: Apps }
+    ];
+
     return (
         <>
             {links.map((link) => {
---
+++ JavaScript/nextjs-app-mui-form/src/app/form/page.tsx
@@ -0,0 +1,11 @@
+export default function Form() {
+    return (
+        <main className="flex flex-col items-center justify-between p-4">
+            <div className="max-w-5xl w-full flex justify-center text-3xl">
+                <h1 className="flex w-full justify-center p-2 lg:p-4">
+                    Form Page
+                </h1>
+            </div>
+        </main>
+    );
+}
