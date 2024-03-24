7563391af03e4b9d1821d284f9a3c2171bca0576
HiLow <newbie_wisdom@hotmail.com>
2024-03-24 10:14:07 +0900

    update

--- JavaScript/nextjs-study/src/app/form/page.tsx
+++ JavaScript/nextjs-study/src/app/form/page.tsx
@@ -1,38 +1,89 @@
 'use client';
 
-import { FormControl, TextField } from '@mui/material';
+import { Button, FormControl, TextField } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 
 export default function Form() {
-    const { control, handleSubmit } = useForm();
+    const { control, handleSubmit } = useForm({ mode: 'onBlur' });
 
     return (
         <main className="w-full flex justify-center">
             <div className="flex flex-col w-full max-w-5xl text-3xl">
                 <h1 className="flex justify-center p-2 lg:p-4">Form Page</h1>
-                <div className="flex flex-col p-4">
-                    <FormControl>
-                        <Controller
-                            name="text"
-                            control={control}
-                            defaultValue=""
-                            rules={{
-                                required: { value: true, message: '必須入力' }
-                            }}
-                            render={({ field, formState: { errors } }) => (
-                                <TextField
-                                    {...field}
-                                    label="名前"
-                                    fullWidth
-                                    placeholder="名無し"
-                                    variant="standard"
-                                    error={errors.text ? true : false}
-                                    helperText={errors.text?.message as string}
-                                />
-                            )}
-                        />
-                    </FormControl>
-                </div>
+                <FormControl component="form" className="flex flex-col gap-4" onSubmit={handleSubmit((data) => console.log(data))}>
+                    <Controller
+                        name="name"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                            required: { value: true, message: '入力が必須です' }
+                        }}
+                        render={({ field, formState: { errors } }) => (
+                            <TextField
+                                {...field}
+                                label="名前 *"
+                                fullWidth
+                                placeholder="名無し"
+                                variant="standard"
+                                error={errors.name ? true : false}
+                                helperText={errors.name?.message as string}
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="email"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                            required: { value: true, message: '入力が必須です' },
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: '不正なメールアドレスです'
+                            }
+                        }}
+                        render={({ field, formState: { errors } }) => (
+                            <TextField
+                                {...field}
+                                label="メールアドレス *"
+                                type="email"
+                                fullWidth
+                                placeholder=""
+                                variant="standard"
+                                error={errors.email ? true : false}
+                                helperText={errors.email?.message as string}
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="password"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                            required: { value: true, message: '入力が必須です' },
+                            minLength: {
+                                value: 8,
+                                message: '8文字以上の入力が必須です'
+                            }
+                        }}
+                        render={({ field, formState: { errors } }) => (
+                            <TextField
+                                {...field}
+                                label="パスワード *"
+                                type="password"
+                                fullWidth
+                                placeholder=""
+                                variant="standard"
+                                error={errors.password ? true : false}
+                                helperText={errors.password?.message as string}
+                            />
+                        )}
+                    />
+                    <div className="flex justify-center">
+                        <Button type="submit" variant="contained" size="large">
+                            送信
+                        </Button>
+                    </div>
+                </FormControl>
             </div>
         </main>
     );
