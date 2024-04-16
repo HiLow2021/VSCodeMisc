7ebf50d7fa74097dbb1e69c22c8c1be95b63278b
HiLow <newbie_wisdom@hotmail.com>
2024-04-17 01:08:43 +0900

    clean

--- JavaScript/nuxtjs-authentication-sidebase-nuxtauth/pages/index.vue
+++ JavaScript/nuxtjs-authentication-sidebase-nuxtauth/pages/index.vue
@@ -3,7 +3,7 @@ definePageMeta({ auth: false });
 </script>
 
 <template>
-    <div class="flex w-full flex-col justify-center items-center">
+    <div class="flex w-full flex-col items-center justify-center">
         <h1 class="mt-4 p-4 text-3xl">Nuxt.js Authentication with Sidebase/Nuxt-Auth.js</h1>
         <div class="flex h-screen items-center justify-center">
             <NuxtLink to="/login" class="flex h-12 w-28 items-center justify-center rounded-md bg-green-500 hover:bg-green-400">
