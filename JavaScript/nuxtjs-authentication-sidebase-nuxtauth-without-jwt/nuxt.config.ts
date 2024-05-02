00aa1ca5a11efd73708a2e00f361638c24de9239
HiLow <newbie_wisdom@hotmail.com>
2024-05-02 21:06:26 +0900

    clean

--- JavaScript/nuxtjs-authentication-sidebase-nuxtauth-without-jwt/nuxt.config.ts
+++ JavaScript/nuxtjs-authentication-sidebase-nuxtauth-without-jwt/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@sidebase/nuxt-auth', "@nuxtjs/tailwindcss"],
+    modules: ['@sidebase/nuxt-auth', '@nuxtjs/tailwindcss'],
     auth: {
         globalAppMiddleware: true,
         provider: {
