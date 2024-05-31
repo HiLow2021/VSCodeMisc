ab66a9f00058bc732b0925969e01293d16c431dc
HiLow <newbie_wisdom@hotmail.com>
2024-05-31 14:27:36 +0900

    update

--- Dart/deep_copy/test/_test.dart
+++ Dart/deep_copy/test/_test.dart
@@ -2,6 +2,22 @@ import 'package:deep_copy/lib.dart';
 import 'package:test/test.dart';
 
 void main() {
+  group('Map', () {
+    final original1 = {'id': 1, 'name': 'Alice'};
+    final original2 = {'id': 2, 'name': 'betty'};
+
+    test('Success', () {
+      final source = {'id': 1, 'name': 'Alice'};
+
+      final Map<String, Object> copy = Map.from(source);
+
+      copy['id'] = 2;
+      copy['name'] = 'betty';
+
+      expect([source, copy], [original1, original2]);
+    });
+  });
+
   group('1D Array', () {
     final original1 = [0, 1, 2];
     final original2 = [999, 1, 2];
