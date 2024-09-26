498edae7bcd6094f227bdc2689214e6f23c31292
HiLow <newbie_wisdom@hotmail.com>
2024-09-26 19:39:02 +0900

    update

--- JavaScript/nestjs-blob/back/src/main.ts
+++ JavaScript/nestjs-blob/back/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(bodyParser.json({ limit: '50mb' }));
+
     await app.listen(5000);
 }
 bootstrap();
