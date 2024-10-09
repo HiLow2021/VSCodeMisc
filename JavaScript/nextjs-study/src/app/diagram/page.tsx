29e4d05a9f7759e74db046b7f50fd1cc9b747a65
HiLow <newbie_wisdom@hotmail.com>
2024-10-09 22:21:19 +0900

    update

--- JavaScript/nextjs-study/src/app/diagram/page.tsx
+++ JavaScript/nextjs-study/src/app/diagram/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { Button } from '@mui/material';
+import html2canvas from 'html2canvas';
 import mermaid from 'mermaid';
 import { useEffect, useState } from 'react';
 
@@ -24,7 +27,7 @@ export default function Diagram() {
             <div className="flex w-full max-w-5xl flex-col gap-8 text-3xl">
                 <h1 className="flex w-full justify-center p-2 lg:p-4">Diagram Page</h1>
                 <div className="flex flex-col items-center gap-8">
-                    <pre className="mermaid flex w-full justify-center">
+                    <pre id="capture" className="mermaid flex w-full justify-center">
                         {`
                         graph TD
                         A[Client] --> B[Load Balancer]
@@ -40,6 +43,28 @@ export default function Diagram() {
                         bar [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]
                         line [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]`}
                     </pre>
+                </div>
+                <div className="flex justify-center pb-8">
+                    <Button
+                        className="w-40"
+                        variant="contained"
+                        component="span"
+                        startIcon={<FileDownloadIcon />}
+                        onClick={async () => {
+                            const element = document.querySelector('#capture');
+                            if (element) {
+                                const canvas = await html2canvas(element as HTMLElement);
+                                const image = canvas.toDataURL('image/png');
+
+                                const link = document.createElement('a');
+                                link.href = image;
+                                link.download = 'diagram.png';
+                                link.click();
+                            }
+                        }}
+                    >
+                        ダウンロード
+                    </Button>
                 </div>
             </div>
         </main>
