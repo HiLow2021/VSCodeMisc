66ca5d0deeb1943f473dc76fb6522704f8e17e9f
HiLow <newbie_wisdom@hotmail.com>
2024-10-09 22:51:56 +0900

    update

--- JavaScript/nextjs-study/src/app/diagram/page.tsx
+++ JavaScript/nextjs-study/src/app/diagram/page.tsx
@@ -36,10 +36,21 @@ export default function Diagram() {
                     </pre>
                     <pre className="mermaid flex w-full justify-center">
                         {`
+                        ---
+                        config:
+                            xyChart:
+                                width: 700
+                                height: 500
+                            themeVariables:
+                                xyChart:
+                                    backgroundColor: "transparent"
+                                    plotColorPalette: "skyblue"
+                        ---
+                        
                         xychart-beta
                         title "Sales Revenue"
                         x-axis [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
-                        y-axis "Revenue (in $)" 4000 --> 11000
+                        y-axis 4000 --> 11000
                         bar [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]
                         line [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]`}
                     </pre>
