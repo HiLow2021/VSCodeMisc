2e43c23bc4f4b6c700f92047eb928bf67b5f9b71
HiLow <newbie_wisdom@hotmail.com>
2024-10-12 00:19:26 +0900

    update

--- JavaScript/nextjs-study/src/app/chart/page.tsx
+++ JavaScript/nextjs-study/src/app/chart/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { Chart as ChartJS, registerables } from 'chart.js';
 import html2canvas from 'html2canvas';
 import { useEffect, useState } from 'react';
@@ -11,6 +11,10 @@ ChartJS.register(...registerables);
 export default function Chart() {
     const [mounted, setMounted] = useState(false);
 
+    const [title, setTitle] = useState<string>('Title');
+    const [labels, setLabels] = useState<string[]>(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']);
+    const [data, setData] = useState<number[]>([1, 2, 3, 4, 5, 6, 7]);
+
     useEffect(() => {
         setMounted(true);
     }, []);
@@ -18,20 +22,23 @@ export default function Chart() {
     useEffect(() => {
         const canvas = document.getElementById('chart') as HTMLCanvasElement;
         if (canvas) {
-            new ChartJS(canvas, {
+            const chart = new ChartJS(canvas, {
                 type: 'bar',
                 data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    labels,
                     datasets: [
                         {
-                            label: 'Title',
-                            data: [12, 19, 3, 5, 2, 3],
+                            label: title,
+                            data,
                             borderWidth: 1
                         }
                     ]
                 },
                 options: {
                     responsive: true,
+                    animation: {
+                        delay: 3000
+                    },
                     scales: {
                         y: {
                             beginAtZero: true
@@ -39,8 +46,10 @@ export default function Chart() {
                     }
                 }
             });
+
+            return () => chart.destroy();
         }
-    }, [mounted]);
+    }, [mounted, title, labels, data]);
 
     if (!mounted) {
         return <></>;
@@ -52,6 +61,32 @@ export default function Chart() {
                 <h1 className="flex w-full justify-center p-2 lg:p-4">Chart Page</h1>
                 <div className="flex h-full w-full justify-center">
                     <canvas id="chart" />
+                </div>
+                <div className="flex flex-col gap-4 p-4">
+                    <TextField
+                        label="Title"
+                        variant="outlined"
+                        defaultValue={title}
+                        onChange={(e) => {
+                            setTitle(e.target.value);
+                        }}
+                    />
+                    <TextField
+                        label="Label"
+                        variant="outlined"
+                        defaultValue={labels.join(',')}
+                        onChange={(e) => {
+                            setLabels(e.target.value.split(','));
+                        }}
+                    />
+                    <TextField
+                        label="Data"
+                        variant="outlined"
+                        defaultValue={data.join(',')}
+                        onChange={(e) => {
+                            setData(e.target.value.split(',').map((value) => parseInt(value)));
+                        }}
+                    />
                 </div>
                 <div className="flex justify-center pb-8">
                     <Button
