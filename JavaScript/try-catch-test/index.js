d7b008abdf736cc092f8e4abd32352ce55ee10d3
HiLow <newbie_wisdom@hotmail.com>
2024-12-04 19:55:03 +0900

    update

---
+++ JavaScript/try-catch-test/index.js
@@ -0,0 +1,41 @@
+console.log('Promise.all');
+await Promise.all(
+    [1, 2, 3, 4, 5].map(async (x) => {
+        try {
+            if (x === 1) {
+                return;
+            }
+
+            const randomNum = Math.floor(Math.random() * 5) + 1;
+            await new Promise((resolve) => setTimeout(resolve, randomNum * 1000));
+
+            if (x === 3) {
+                throw new Error('error');
+            }
+
+            console.log('try', x);
+        } finally {
+            console.log('finally', x);
+        }
+    })
+);
+
+console.log('for of');
+for (const x of [1, 2, 3, 4, 5]) {
+    try {
+        if (x === 1) {
+            return;
+        }
+
+        const randomNum = Math.floor(Math.random() * 5) + 1;
+        await new Promise((resolve) => setTimeout(resolve, randomNum * 1000));
+
+        if (x === 3) {
+            throw new Error('error');
+        }
+
+        console.log('try', x);
+    } finally {
+        console.log('finally', x);
+    }
+}
