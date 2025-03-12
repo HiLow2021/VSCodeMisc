9d9ffe28ea72578b8fbdcb756f969a633aa8d283
HiLow <newbie_wisdom@hotmail.com>
2025-03-13 02:53:43 +0900

    update

---
+++ JavaScript/png-to-ico/index.js
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import pngToIco from 'png-to-ico';
+import sharp from 'sharp';
+
+const mediaDirectory = './media/';
+const outDirectory = './out/';
+const input = fs.readFileSync(`${mediaDirectory}input.png`);
+
+if (!fs.existsSync(mediaDirectory)) {
+    fs.mkdirSync(mediaDirectory);
+}
+if (!fs.existsSync(outDirectory)) {
+    fs.mkdirSync(outDirectory);
+}
+
+try {
+    const metadata = await sharp(input).metadata();
+    if (metadata.width !== metadata.height) {
+        throw new Error('画像の幅と高さが異なるため、正方形の画像を指定してください');
+    }
+
+    const originalSize = metadata.width;
+    if (originalSize < 32) {
+        throw new Error('32 ピクセル以上の画像を指定してください');
+    }
+
+    let maxSize = 1;
+    while (maxSize * 2 <= originalSize) {
+        maxSize *= 2;
+    }
+
+    const sizes = [originalSize];
+    for (let size = maxSize; size >= 32; size = size / 2) {
+        sizes.push(size);
+    }
+
+    console.log('生成するアイコンサイズ:', sizes.reverse());
+
+    const buffers = await Promise.all(sizes.reverse().map((size) => sharp(input).resize(size, size, { fit: 'contain' }).png().toBuffer()));
+    const icoBuffer = await pngToIco(buffers);
+
+    fs.writeFileSync(`${outDirectory}result.ico`, icoBuffer);
+    console.log('アイコンファイルの生成に成功しました');
+} catch (error) {
+    console.error('エラーが発生しました:', error);
+}
